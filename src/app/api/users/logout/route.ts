7acf38d8d03af41e546ccf0d8f0353aaf4ea79a1
Radheshyam gupta <radheshyamjio52@gmail.com>
2024-04-26 09:48:41 +0530

    complete protected route & complete project

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(request:NextRequest){
+export async function POST(request: NextRequest) {
     try {
         // const response = NextResponse.json({message:"Logout Succesfull"})
 
@@ -17,12 +17,13 @@ export async function POST(request:NextRequest){
                 success: true,
             }
         )
-        response.cookies.set("token", "", 
-        { httpOnly: true, expires: new Date(0) 
-        });
+        response.cookies.set("auth_token", "",
+            {
+                httpOnly: true, expires: new Date(0)
+            });
         return response;
-    } catch (error:any) {
-        return NextResponse.json({message:error.message,status:500})
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message, status: 500 })
         console.log(error)
     }
 }
